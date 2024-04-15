f355ea29d2e25d496dfd4ea769d2583503972db9
caokun <caokun@163.com>
2024-04-15 21:33:23 +0800

    first commit

---
+++ src/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,19 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+public class ID1TwoSum{
+	public static void main(String[] args) {
+		Solution solution = new ID1TwoSum().new Solution();
+		
+		
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
